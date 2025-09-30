66a7599e80bfd33e81967f4c56abdad031da2084
Kushagra Gupta <kushgupta118@gmail.com>
2025-09-30 19:36:38 +0530

    initial

---
+++ dif.java
@@ -0,0 +1,8 @@
+package program;
+
+public class dif {
+    public static void main(String[] args) {
+        int b=10;
+        System.out.println("the sum is:"+ b);
+    }
+}
---
+++ hy.java
@@ -0,0 +1,8 @@
+package program;
+
+public class hy {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println("the sum is:"+a);
+    }
+}
